ffec2b348723f353583be11d568f85795930bacc
subscriberngkeanyik-rgb <subscriberngkeanyik@ipk.edu.my>
2025-12-29 10:59:44 +0800

    Update test_calculator.cpp

--- test_calculator.cpp
+++ test_calculator.cpp
@@ -1,10 +1,10 @@
 #include <gtest/gtest.h>
 #include "calculator.h"
 
-TEST(CalculatorTest, AddTest){
-  EXPECT_EQ(add(2, 3), 5);
+TEST(CalculatorTest, AddTest) {
+EXPECT_EQ(add(2, 3), 5);
 }
 
-TEST(CalculatorTest, AddTest){
-  EXPECT_EQ(subtract(5,3), 2);
+TEST(CalculatorTest, SubtractTest) {
+EXPECT_EQ(subtract(5, 3), 2);
 }
